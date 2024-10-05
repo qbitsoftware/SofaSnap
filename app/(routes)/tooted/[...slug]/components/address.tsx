4cb25f0b07af81cf2d309b6fd4fed4611752fda7
Kristo Veber <kristoveber7@gmail.com>
2024-10-05 14:24:32 +0300

    refactor

--- app/(routes)/tooted/[...slug]/components/address.tsx
+++ app/(routes)/tooted/[...slug]/components/address.tsx
@@ -13,7 +13,7 @@ const AddressComponent: React.FC<AddressProps> = ({ address, className }) => {
     return (
         <>
             <div className={cn('px-6 md:px-[64px] h-[218px] md:h-[480px] max-w-[1440px] mx-auto md:mt-[200px]', className)}>
-                <GoogleMapComponent className={""} api={process.env.GOOGLE_MAPS_KEY!} markers={[{ lat: address.location.y, lng: address.location.x }]} />
+                <GoogleMapComponent api={process.env.GOOGLE_MAPS_KEY!} markers={[{ lat: address.location.y, lng: address.location.x }]} />
             </div>
             <div className='bg-[#CBD3CB]/35 md:h-[120px] mt-3'>
                 <div className='h-full max-w-[1440px] px-[64px] mx-auto md:flex md:flex-col md:justify-center gap-1'>
