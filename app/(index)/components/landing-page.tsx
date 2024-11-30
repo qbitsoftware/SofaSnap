076c4a2fa1930773cd194bcce88bc414372147e3
Kristo Veber <kristoveber7@gmail.com>
2024-11-30 19:07:23 +0200

    fix products route

--- app/(index)/components/landing-page.tsx
+++ app/(index)/components/landing-page.tsx
@@ -94,7 +94,7 @@ export const LandingPage = ({ products }: Props) => {
                         <h1 className={cn('hidden md:block md:text-4xl lg:text-6xl md:w-[50vw] md:max-w-[553px] h-full font-semibold absolute z-10 text-start', MontserratAlternates.className)}>Rentige mööblit oma<br className='lg:hidden' /> piirkonna inimestelt<br className='lg:hidden' /> </h1>
                         <h1 className={cn('md:hidden text-center text-2xl sm:text-4xl font-semibold', MontserratAlternates.className)}>Anna mööblile uus elu -<br></br>teenid sina, võidab loodus!</h1>
                     </div>
-                    <Link className='mx-auto z-10' href={"/lisa-toode"} >
+                    <Link className='mx-auto z-10' href={"/tooted"} >
                         <Button className='md:bg-accent text-[#F07162] bg-transparent md:text-foreground hover:bg-transparent rounded-3xl md:hover:bg-[#f07162] md:w-[180px] md:h-[60px] mt-5 md:mt-0 underline md:no-underline'>
                             Vaata tooteid
                         </Button>
